b23ab7214e0c0f08bdaebee6ca07c1ac60738a8e
Zhenyi-Liu <zhenyiliu27@gmail.com>
2021-01-08 18:47:14 +0800

    add changes

--- src/pbrt/cpu/integrators.cpp
+++ src/pbrt/cpu/integrators.cpp
@@ -264,14 +264,6 @@ void ImageTileIntegrator::Render() {
                 camera.GetFilm().WriteImage(metadata, 1.0f / waveStart);
             }
         }
-<<<<<<< HEAD
-        camera.InitMetadata(&metadata);
-        // only write out data at find spp; --zhenyi
-        // we might set another global flag to check whether we are writing out basis        
-        if (waveStart == spp)
-            camera.GetFilm().WriteImage(metadata, 1.0f / waveStart);
-=======
->>>>>>> upstream/master
     }
 
     if (mseOutFile)
